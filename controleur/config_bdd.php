543761ff60ec7a957e6fa030868e424a836d6d83
Skuskusku13 <danlevy149@gmail.com>
2022-05-27 14:20:11 +0200

    new pages

--- controleur/config_bdd.php
+++ controleur/config_bdd.php
@@ -1,13 +1,7 @@
 <?php
     //Connex BDD Mac
-    /*$serveur = "localhost:8889";
+    $serveur = "localhost:8889";
     $bdd = "jo_paris";
     $user = "root";
-    $mdp = "root";*/
-
-    //Connex BDD PC
-    $serveur = "localhost";
-    $bdd = "garage_250_1";
-    $user = "root";
-    $mdp = "";
+    $mdp = "root";
 ?>
